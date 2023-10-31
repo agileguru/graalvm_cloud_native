af516122a9f0e8f58ea46563e707e8bd8aa85365
Guru Raghupathy <guru@doit-intl.com>
2023-10-31 10:32:11 +0000

    Initial Commit

---
+++ graalvmcloudnativeclient/src/main/java/com/doit/agileguru/graalvmcloudnativeclient/GraalvmcloudnativeclientApplication.java
@@ -0,0 +1,13 @@
+package com.doit.agileguru.graalvmcloudnativeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmcloudnativeclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmcloudnativeclientApplication.class, args);
+	}
+
+}
