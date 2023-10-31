1b3435a23403fd51136486501a0e8b449fdd4c7f
Guru Raghupathy <guru@doit-intl.com>
2023-10-31 09:00:59 +0000

    Added Working Server

---
+++ graalvmcloudnativeserver/src/main/java/com/doit/demo/graalvmcloudnativeserver/GraalvmcloudnativeserverApplication.java
@@ -0,0 +1,13 @@
+package com.doit.demo.graalvmcloudnativeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmcloudnativeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmcloudnativeserverApplication.class, args);
+	}
+
+}
---
+++ graalvmcloudnativeserver/src/test/java/com/doit/demo/graalvmcloudnativeserver/GraalvmcloudnativeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doit.demo.graalvmcloudnativeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmcloudnativeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
